8f9d8073851d337db8047d9a23a3bc5004c19c22
Priscilabiazotti <priscilabiazotti@gmail.com>
2023-04-05 16:53:19 -0300

    java teste

--- curso_programacao/src/Main.java
+++ curso_programacao/src/Main.java
@@ -1,9 +1,27 @@
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
 
-		System.out.println("Olá Mundo!");
+		Scanner sc = new Scanner(System.in);
+		int hora;
+				
+		System.out.println("quantas horas?");
+		hora = sc.nextInt();
+		
+		if (hora < 12) {
+		    System.out.println("bom dia");
+		}
+		
+		else if (hora < 18) {
+	         System.out.println("boa tarde");
+		}
+		else {	
+	         System.out.println("boa noite");						
+		}
+		sc.close();
+
 	}
 
 }
