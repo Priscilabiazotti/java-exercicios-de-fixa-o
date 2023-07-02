c8b7deb63f201efe4bb8a6e05ad02f0c52ecca7d
Priscilabiazotti <priscilabiazotti@gmail.com>
2023-07-02 13:12:54 -0300

    exercicio

---
+++ curso_programacao/src/estruturarepetitiva/exercicio.java
@@ -0,0 +1,22 @@
+package estruturarepetitiva;
+
+import java.util.Scanner;
+
+public class exercicio {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner sc = new Scanner(System.in);
+
+		int x = sc.nextInt();
+
+		while (x != 0) {
+			x = sc.nextInt();
+		}
+
+		sc.close();
+
+	}
+
+}
